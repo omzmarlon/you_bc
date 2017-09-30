1701f51154febe390187a1e80a9b9f70a30109f9
Marlon Ou <omzmarlon@gmail.com>
2017-09-30 15:56:39 -0700

    Pass any security

--- src/main/java/com/pokecabin/securities/PokeCabinSecurity.java
+++ src/main/java/com/pokecabin/securities/PokeCabinSecurity.java
@@ -2,6 +2,7 @@ package com.pokecabin.securities;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,7 +14,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class PokeCabinSecurity extends WebSecurityConfigurerAdapter {
     @Override
-    public void configure(AuthenticationManagerBuilder authBuilder) {
+    public void configure(AuthenticationManagerBuilder authBuilder) throws Exception {
+        authBuilder.inMemoryAuthentication()
+                .withUser("marlon")
+                .password("omzmarlon").roles("admin");
+    }
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
     }
 }
