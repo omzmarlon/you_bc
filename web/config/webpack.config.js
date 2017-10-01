c0263aa5ecf1f483fcaf214c9fe4eda5d20ecd70
tomyang729 <tomatoyang729@gmail.com>
2017-09-30 17:22:41 -0700

    front end set up

---
+++ web/config/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const helpers = require('./helpers');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: helpers.root('..', 'src', 'main', 'resources', 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+}
