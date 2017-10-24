0bea367bafc7a8f5aa5f1805d38caea40b82c5a1
Marlon Ou <omzmarlon@gmail.com>
2017-10-24 13:49:48 -0700

    rename

--- src/main/java/com/pokecabin/PokeCabinApplication.java
+++ src/main/java/com/pokecabin/PokeCabinApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PokeCabin {
+public class PokeCabinApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PokeCabin.class, args);
+        SpringApplication.run(PokeCabinApplication.class, args);
     }
 }
