ebfd506df6a7fde96eab335dc1ea0293548132a1
tomyang729 <tomatoyang729@gmail.com>
2017-11-23 14:17:28 -0800

    revisit webpack config, add loaders and HMR

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -1,34 +1,68 @@
 const path = require('path');
 const helpers = require('./helpers');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './src/index.html',
-    filename: 'index.html',
-    inject: 'body'
-});
+const dist = '../../src/main/resources/public';
 
 module.exports = {
-    entry: './src/index.js',
+    devtool: 'cheap-eval-source-map',
+    entry: [
+        // 'babel-polyfill',
+        'react-hot-loader/patch',
+        './src/index.js'
+    ],
     output: {
         path: helpers.root('..', 'src', 'main', 'resources', 'public'),
         filename: 'bundle.js'
     },
+    devServer: {
+        contentBase: dist,
+        port: 8081,
+        compress: true,
+        hot: true
+    },
     module: {
-        loaders: [
+        rules: [
             {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                query: {
-                    presets: ['env','react']
-                }
-            }
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
         ]
     },
-    plugins: [HtmlWebpackPluginConfig],
-    devServer: {
-        port: 8081,
-        compress: true
+    plugins: [
+        new CleanWebpackPlugin([dist]),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
     }
 };
