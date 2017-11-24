410d0de29ea9de8b13a33601cb909ebdf98412d9
tomyang729 <tomatoyang729@gmail.com>
2017-11-24 11:28:53 -0800

    fixed minor bugs

--- web/config/webpack.prod.js
+++ web/config/webpack.prod.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
     plugins: [
