968651ec2e705b4c97bd6276707e4a37820ce9c2
Marlon Ou <omzmarlon@gmail.com>
2017-12-07 13:58:33 -0800

    Implemented NavHeader A few notes: 1. I also included IconButton and Icon dummy components in this commit so that I can show what the header actually looks like 2. we had a trello card to implement IconButton dummy component, but the great MaterialUI’s already done it for us, so we don’t have to implement it ourselves. 3. Material UI has an SvgIcon component, but it kinda wants SVG written into Javascript, which is not good for us I think(we can just use a img tag to include a SVG static file, which is much more convenient.) So I wrote a Icon component just as a thin wrapper for all our icons(.svg, .png, etc) 4. Fixed a typo in Block.js 5. Since our NavHeader always has “Go Back“ button as default, so it is written into NavHeader. The actionRight is still left open, because we sometimes need it sometimes don’t

--- web/src/components/demo/Demo.js
+++ web/src/components/demo/Demo.js
@@ -2,26 +2,25 @@
 
 // libs
 import React, {Component} from 'react'
-import Header from "../common/Header";
-import {PRIMARY_BLUE} from "../../styles/constants/colors";
-// components
+import NavHeader from "../common/NavHeader";
+import {PRIMARY_BLUE, PRIMARY_YELLOW} from "../../styles/constants/colors";
+import IconButton from 'material-ui/IconButton';
+import Icon from "../common/Icon";
 
-// styles
+import DemoAndroidIcon from '../../../public/svg/DemoAndroidIcon.svg';
 
 class Demo extends Component {
     render() {
         return (
             <div>
-                <Header
+                <div>this string should be covered by header</div>
+                <NavHeader
                     title={"曾今的人"}
-                    actionLeft={<button>abc</button>}
                     color={PRIMARY_BLUE}
-                    actionRight={<button>abc</button>}
-                />
-                <Header
-                    title={"曾今的人"}
-                    actionLeft={<button>abc</button>}
-                    color={PRIMARY_BLUE}
+                    // NOTE:
+                    // A demo on how to use actionRight
+                    // we should keep our svg files in public/svg folder
+                    actionRight={<IconButton><Icon src={DemoAndroidIcon}/></IconButton>}
                 />
             </div>
         );
