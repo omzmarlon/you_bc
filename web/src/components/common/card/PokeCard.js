c1e0b11b7f5d603bf7d214b45b71010cd74faef8
tomyang729 <tomatoyang729@gmail.com>
2017-12-14 00:14:01 -0800

    demo change && pokecard padding change

--- web/src/components/common/card/PokeCard.js
+++ web/src/components/common/card/PokeCard.js
@@ -8,10 +8,10 @@ import "./PokeCard.less";
 const PokeCard = (props) => {
     return (
         <Paper>
-            <div className="poke-card-head">
+            {(props.leftCorner || props.rightCorner) && <div className="poke-card-head">
                 {props.leftCorner}
                 {props.rightCorner && <span className="poke-card-head-right-icon">{props.rightCorner}</span>}
-            </div>
+            </div>}
             {props.children}
         </Paper>
     );
--- web/src/components/demo/Demo.js
+++ web/src/components/demo/Demo.js
@@ -2,7 +2,6 @@
 
 // libs
 import React, {Component} from 'react'
-import IconButton from 'material-ui/IconButton';
 // components
 import NavHeader from "../common/NavHeader";
 import Tag from "../common/Tag";
@@ -14,6 +13,7 @@ import MatchedUserCard from '../common/card/MatchedUserCard';
 import ProfileCard from '../common/card/ProfileCard';
 import InfoBar from "../common/InfoBar";
 import Modal from "../common/Modal";
+import Slidable from "../common/Slidable";
 // assets
 import DemoAndroidIcon from '../common/svg/DemoAndroidIcon';
 import avatar from "../../../public/panda_avatar.jpeg";
@@ -22,10 +22,8 @@ import SchoolIcon from 'material-ui/svg-icons/social/school';
 import DraftIcon from 'material-ui/svg-icons/content/drafts';
 import MailIcon from 'material-ui/svg-icons/content/mail';
 // constants
-import {PRIMARY_BLUE, PRIMARY_GREEN, SECONDARY_GREEN, PRIMARY_YELLOW, PRIMARY_RED, SECONDARY_RED} from "../../styles/constants/colors";
+import {PRIMARY_BLUE, PRIMARY_GREEN, SECONDARY_GREEN, PRIMARY_YELLOW, PRIMARY_WHITE} from "../../styles/constants/colors";
 import {CLASSMATES, ROOMMATES, FRIENDS} from '../../constants/api';
-import InfoBar from "../common/InfoBar";
-import Slidable from "../common/Slidable";
 //styles
 import "../../styles/constants/fonts.less";
 import "./Demo.less"
@@ -53,116 +51,77 @@ class Demo extends Component {
                     color={PRIMARY_BLUE}
                     iconRight={<DemoAndroidIcon color={PRIMARY_WHITE} className={"common-icon"}/>}
                 />
-                <div>Tag Demo</div>
-                <div style={{margin:28}}>
-                    <Tag text={"计算机"} bkgColor={SECONDARY_GREEN} textColor={PRIMARY_GREEN}/>
-                </div>
-                <br/>
-                <br/>
-                <br/>
-                <hr/>
-                <div>Avatar Demo</div>
-                <div style={{margin:28}}>
-                    <PokeAvatar img={avatar}/>
-                    <PokeAvatar img={avatar} name='张无忌'/>
-                    <PokeAvatar
-                        img={avatar}
-                        name="杨过"
-                        info={{
-                            gender: 'male',
-                            age: 26,
-                            constellation: '射手座'
-                        }}/>
-                </div>
-                <br/>
-                <br/>
-                <br/>
-                <hr/>
-                <div>Card Demo</div>
-                <div style={{margin:28}}>
-                    <SearchResultCard
-                        avatar={placeholder}
-                        name="小傻瓜"
-                        gender='male'
-                        age={8}
-                        constellation='电插座'
-                        contentList={[
-                            {
-                                leftElement: <SchoolIcon color={PRIMARY_YELLOW}/>,
-                                rightElement: <TruncateText style={{paddingLeft: 16}} text="汽修专业 大一" />
-                            },
-                            {
-                                leftElement: <DraftIcon color={PRIMARY_YELLOW}/>,
-                                rightElement: <span style={{paddingLeft: 16}}>好好学习</span>
-                            },
-                            {
-                                leftElement: <MailIcon color={PRIMARY_YELLOW}/>,
-                                rightElement: <span style={{paddingLeft: 16}}>我其实是个学霸</span>
-                            },
-                            {
-                                leftElement: <span>兴趣: </span>,
-                                rightElement: <TruncateText textStyle={{width: 300}} style={{paddingLeft: 16}} text="这个超长的这个超长的这个超长的这个超长的这个超长的这个超长的这个超长的这个超长的"/>
-                            }
-                        ]}
-                    />
-                </div>
-                <div style={{margin:28}}>
-                    <ProfileCard
-                        type={CLASSMATES}
-                        contentList={[
-                            {
-                                leftElement: <span>专业</span>,
-                                rightElement: <TruncateText style={{paddingLeft: 16}} text="汽修专业 大一" />
-                            },
-                            {
-                                leftElement: <span>课程</span>,
-                                rightElement: <div style={{paddingLeft: 16}}><Tag text='CPSC 310' bkgColor={SECONDARY_GREEN} textColor={PRIMARY_GREEN}/></div>
-                            },
-                            {
-                                leftElement: <span>能力</span>,
-                                rightElement: <span style={{paddingLeft: 16}}>我其实是个学霸</span>
-                            },
-                            {
-                                leftElement: <span>兴趣: </span>,
-                                rightElement: <TruncateText textStyle={{width: 300}} style={{paddingLeft: 16}} text="这个超长的这个超长的这个超长的这个超长的这个超长的这个超长的这个超长的这个超长的"/>
-                            }
-                        ]}
-                    />
-                </div>
-                <div style={{margin:28}}>
-                    <MatchedUserCard
-                        type={FRIENDS}
-                        avatar={avatar}
-                        name="冬瓜"
-                        weChatId="donggua"
-                        tags={['吃吃吃', '买买买', '啪啪啪']}
-                    />
-                </div>
-                <br/>
-                <br/>
-                <br/>
-                <hr/>
-                <div>Modal Demo</div>
                 <div>Slidable demo</div>
                 <Slidable element={
-                    <div style={{
-                        width: '100%',
-                        height: '300px',
-                        backgroundColor: 'red',
-                        display:'flex', justifyContent: 'center', alignItems: 'center'
-                    }}
-                    >
-                        <span className={'common-font'}>
-                            滑动样本，可以左滑右滑。
-                            如果滑动较小会自动复位，如果滑动超过threshold会自动滑出100%
-                        </span>
+                    <div style={{margin:28}}>
+                        <SearchResultCard
+                            avatar={placeholder}
+                            name="小傻瓜"
+                            gender='male'
+                            age={8}
+                            constellation='电插座'
+                            contentList={[
+                                {
+                                    leftElement: <SchoolIcon color={PRIMARY_YELLOW}/>,
+                                    rightElement: <TruncateText style={{paddingLeft: 16}} text="汽修专业 大一" />
+                                },
+                                {
+                                    leftElement: <DraftIcon color={PRIMARY_YELLOW}/>,
+                                    rightElement: <span style={{paddingLeft: 16}}>好好学习</span>
+                                },
+                                {
+                                    leftElement: <MailIcon color={PRIMARY_YELLOW}/>,
+                                    rightElement: <span style={{paddingLeft: 16}}>我其实是个学霸</span>
+                                },
+                                {
+                                    leftElement: <span>兴趣: </span>,
+                                    rightElement: <TruncateText textStyle={{width: 300}} style={{paddingLeft: 16}} text="这个超长的这个超长的这个超长的这个超长的这个超长的这个超长的这个超长的这个超长的"/>
+                                }
+                            ]}
+                        />
+                    </div>
+                }/>
+                <Slidable element={
+                    <div style={{margin:28}}>
+                        <ProfileCard
+                            type={CLASSMATES}
+                            contentList={[
+                                {
+                                    leftElement: <span>专业</span>,
+                                    rightElement: <TruncateText style={{paddingLeft: 16}} text="汽修专业 大一" />
+                                },
+                                {
+                                    leftElement: <span>课程</span>,
+                                    rightElement: <div style={{paddingLeft: 16}}><Tag text='CPSC 310' bkgColor={SECONDARY_GREEN} textColor={PRIMARY_GREEN}/></div>
+                                },
+                                {
+                                    leftElement: <span>能力</span>,
+                                    rightElement: <span style={{paddingLeft: 16}}>我其实是个学霸</span>
+                                },
+                                {
+                                    leftElement: <span>兴趣: </span>,
+                                    rightElement: <TruncateText textStyle={{width: 300}} style={{paddingLeft: 16}} text="这个超长的这个超长的这个超长的这个超长的这个超长的这个超长的这个超长的这个超长的"/>
+                                }
+                            ]}
+                        />
+                    </div>
+                }/>
+                <Slidable element={
+                    <div style={{margin:28}}>
+                        <MatchedUserCard
+                            type={FRIENDS}
+                            avatar={avatar}
+                            name="冬瓜"
+                            weChatId="donggua"
+                            tags={['吃吃吃', '买买买', '啪啪啪']}
+                        />
                     </div>
                 }/>
 
-                <InfoBar msg={"展示如果很多字会不会换行这个是提示框样本"} show={true}/>
-                <button onClick={this.toggleOpenModal}>
+                {/*<InfoBar msg={"展示如果很多字会不会换行这个是提示框样本"} show={true}/>*/}
+                <RaisedButton onClick={this.toggleOpenModal}>
                     show modal
-                </button>
+                </RaisedButton>
 
                 <Modal open={this.state.openModal}>
                     <div>some text</div>
