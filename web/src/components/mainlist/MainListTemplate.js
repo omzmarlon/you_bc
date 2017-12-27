9a4fd27a99be95cd321330a6700c10b3d5a73866
tomyang729 <tomatoyang729@gmail.com>
2017-12-26 20:18:49 -0800

    finish MainListTemplate

--- web/src/components/mainlist/MainListTemplate.js
+++ web/src/components/mainlist/MainListTemplate.js
@@ -7,73 +7,17 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import MainListHeader from './MainListHeader'
 import SearchResultCard from '../../components/common/card/SearchResultCard'
 import Slidable from '../../components/common/Slidable'
-import {CourseInfo, MajorInfo, StudyAbilityInfo, StudyRequirementInfo,} from './ContentLists';
+import {
+    AddressInfo, CourseInfo, FacultyInfo, HobbyInfo, HometownInfo, MajorInfo, MottoInfo, RelationshipInfo,
+    StudyAbilityInfo,
+    StudyRequirementInfo,
+} from './ContentLists';
 // assets
 import HappyFaceImg from '../../components/common/svg/HappyFaceImg'
 import CryFaceImg from '../../components/common/svg/CryFaceImg'
 // styles
-import {PRIMARY_BLUE, PRIMARY_GREEN, PRIMARY_RED, PRIMARY_WHITE, SECONDARY_RED} from '../styles/constants/colors'
 import './MainListTemplate.less'
 
-const hardCodeData = [
-    {
-        avatar: avatar1,
-        name: '驴打滚',
-        gender: 'female',
-        age: 20,
-        constellation: '处女座',
-        matchRate: 0.8,
-        major: 'Marketing',
-        year: 'III',
-        courses: ['COMM296', 'COMM294'],
-        description: '我是一个学霸',
-        requirements: ['自习', '上课', '同桌']
-    },
-    {
-        avatar: avatar2,
-        name: '艾窝窝',
-        gender: 'male',
-        age: 23,
-        constellation: '天蝎座',
-        major: 'Finance',
-        year: 'IV',
-        courses: ['COMM298', 'COMM488'],
-        description: '我是一个学霸',
-        requirements: ['自习', '上课']
-    },
-    {
-        avatar: avatar3,
-        name: '麻花',
-        gender: 'female',
-        age: 21,
-        constellation: '处女座',
-        matchRate: 0.8,
-        major: 'Marketing',
-        year: 'III',
-        courses: ['COMM296', 'COMM294'],
-        description: '我是一个学霸',
-        requirements: ['自习', '上课', '同桌']
-    },
-    {
-        avatar: avatar4,
-        name: '红烧肉',
-        gender: 'female',
-        age: 19,
-        constellation: '摩羯座',
-        matchRate: 0.8,
-        major: 'Marketing',
-        year: 'V',
-        courses: ['COMM296', 'COMM294'],
-        description: '我是一个学霸',
-        requirements: ['自习', '上课', '同桌']
-    }
-];
-
-const iconStyle = {
-    width: 14,
-    height: 14,
-    color: PRIMARY_RED
-};
 
 const CardTransition = ({ children, ...props }) => (
     <CSSTransition
@@ -86,11 +30,9 @@ const CardTransition = ({ children, ...props }) => (
 );
 
 class MainListTemplate extends Component {
-    // state: 1. all data; 2. current three ones to display
     constructor(props) {
         super(props);
         this.state = {
-            userPool: [],
             displayUsers: []
         };
         this.cardUpdateHandler = this.cardUpdateHandler.bind(this);
@@ -99,8 +41,7 @@ class MainListTemplate extends Component {
     // get data
     componentDidMount() {
         this.setState({
-            userPool: hardCodeData,
-            displayUsers: hardCodeData.slice(0, 3)
+            displayUsers: this.props.userData.slice(0, 3)
         });
     }
 
@@ -110,26 +51,26 @@ class MainListTemplate extends Component {
             displayUsers: [
                 ...this.state.displayUsers.slice(0, index),
                 ...this.state.displayUsers.slice(index+1, 3),
-                hardCodeData[3]
+                this.props.userData[3] // TODO: how to handle the user list? keep looping? what if run out of users?
             ]
         });
     }
 
     render() {
         return(
-            <div className="classmate-container">
+            <div className="main-list-container">
                 <MainListHeader title={this.props.title} color={this.props.themeColor}/>
-                <TransitionGroup className="classmate-card-list">
+                <TransitionGroup className="main-list-card-list">
                     {this.state.displayUsers.map(
                         (user, index) => (
                             <CardTransition key={user.name}>
-                                <div className="classmate-row-wrapper">
+                                <div className="main-list-row-wrapper">
                                     <span className="--smile">{<HappyFaceImg />}</span>
                                     <span className="--cry">{<CryFaceImg />}</span>
                                     <Slidable
                                         onFullSwipe={() => this.cardUpdateHandler(index)}
                                         element={
-                                            <div className="classmate-card-list-item">
+                                            <div className="main-list-card-list-item">
                                                 <SearchResultCard
                                                     avatar={user.avatar}
                                                     name={user.name}
@@ -139,10 +80,16 @@ class MainListTemplate extends Component {
                                                     matchRate={user.matchRate}
                                                     matchRateColor={this.props.themeColor}
                                                     contentList={[
-                                                        <MajorInfo color={this.props.themeColor} major={user.major} year={user.year}/>,
-                                                        <CourseInfo color={this.props.themeColor} secondColor={this.props.subThemeColor} courses={user.courses}/>,
-                                                        <StudyAbilityInfo description={user.description}/>,
-                                                        <StudyRequirementInfo color={this.props.themeColor} secondColor={this.props.subThemeColor} requirements={user.requirements}/>
+                                                        (user.major && <MajorInfo color={this.props.themeColor} major={user.major} year={user.year}/>),
+                                                        (user.courses && <CourseInfo color={this.props.themeColor} secondColor={this.props.subThemeColor} courses={user.courses}/>),
+                                                        (user.description && <StudyAbilityInfo description={user.description}/>),
+                                                        (user.requirements && <StudyRequirementInfo color={this.props.themeColor} secondColor={this.props.subThemeColor} requirements={user.requirements}/>),
+                                                        (user.address && <AddressInfo color={this.props.themeColor} address={}/>),
+                                                        (user.country && <HometownInfo color={this.props.themeColor} city={user.city} country={user.country}/>),
+                                                        (user.motto && <MottoInfo color={this.props.themeColor} motto={user.motto}/>),
+                                                        (user.hobbies && <HobbyInfo hobbies={user.hobbies} color={this.props.themeColor} secondColor={this.props.subThemeColor}/>),
+                                                        (user.faculty && <FacultyInfo color={this.props.themeColor} faculty={user.faculty} year={user.year}/>),
+                                                        (user.relationship && <RelationshipInfo color={this.props.themeColor} relationship={user.relationship}/>),
                                                     ]}
                                                 />
                                             </div>
@@ -162,12 +109,7 @@ MainListTemplate.propTypes = {
     title: PropTypes.string.isRequired,
     themeColor: PropTypes.string.isRequired,
     subThemeColor: PropTypes.string.isRequired,
-    contentList: PropTypes.arrayOf(
-        PropTypes.shape({
-            leftElement: PropTypes.element,
-            rightElement: PropTypes.element
-        })
-    ).isRequired
+    userData: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default MainListTemplate;
