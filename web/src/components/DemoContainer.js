67fb2145e024c9d1bd625a0af95dc8e54c936e85
tomyang729 <tomatoyang729@gmail.com>
2018-01-06 13:34:41 -0800

    finish survey-complete-modal

--- web/src/components/DemoContainer.js
+++ web/src/components/DemoContainer.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {RaisedButton} from "material-ui";
+import SurveyCompleteModal from "./profile/SurveyCompleteModal";
 
-const DemoContainer = () => {
-    return (
-        <div>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </div>
-    );
-};
+class DemoContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            openSurveyCompleteModal: false
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>
+                        <RaisedButton
+                            label="SurveyCompleteModal"
+                            onClick={() => {this.setState({openSurveyCompleteModal: true})}}
+                        />
+                        <SurveyCompleteModal
+                            onClose={() => {this.setState({openSurveyCompleteModal: false})}}
+                            openModal={this.state.openSurveyCompleteModal}
+                        />
+                    </li>
+
+                </ul>
+            </div>
+        );
+    }
+}
 
 export default DemoContainer;
