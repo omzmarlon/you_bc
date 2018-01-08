aacec8e8d80643fc8d8f520019a47dd03d418f6d
tomyang729 <tomatoyang729@gmail.com>
2018-01-08 02:48:08 -0800

    finish email check container

--- web/src/containers/verification/EmailCheckContainer.js
+++ web/src/containers/verification/EmailCheckContainer.js
@@ -6,26 +6,41 @@ import './EmailCheckContainer.less';
 import {RaisedButton, TextField} from "material-ui";
 import {PRIMARY_GREEN, PRIMARY_WHITE} from "../../styles/constants/colors";
 import EmailImg from "../../components/common/svg/EmailImg";
+import FlatButton from 'material-ui/FlatButton';
 
 const inputStyle = {
     width: '43vw',
-    paddingRight: 5,
+    paddingRight: 5
 };
 
 const underlineStyle = {
     width: '43vw'
 };
 
+const inputStyle2 = {
+    width: '36vw',
+    paddingLeft: 5
+};
+
+const underlineStyle2 = {
+    width: '36vw'
+};
+
 class EmailCheckContainer extends Component {
     constructor(props) {
         super(props);
+        // emailSubmitted should be in redux
         this.state = {
             email: "",
-            editing: true
+            emailSubmitted: false,
+            code: ""
+
         };
         this.onEmailChange = this.onEmailChange.bind(this);
         this.onEmailSubmit = this.onEmailSubmit.bind(this);
+        this.onCodeChange = this.onCodeChange.bind(this);
         this.onCodeSubmit = this.onCodeSubmit.bind(this);
+        this.editEmailHandler = this.editEmailHandler.bind(this);
     }
 
     onEmailChange(e, val) {
@@ -33,12 +48,23 @@ class EmailCheckContainer extends Component {
     }
 
     onEmailSubmit() {
-        this.setState({ editing: false });
+        this.setState({ emailSubmitted: true });
+        console.log('submit email');
         // todo: make post request
     }
 
-    onCodeSubmit() {
+    onCodeChange(e, val) {
+        this.setState({ code: val });
+    }
 
+    onCodeSubmit() {
+        // todo: make post request
+        console.log('submit code');
+    }
+
+    editEmailHandler() {
+        // set code back to null, since we submit email only when code is null
+        this.setState({ emailSubmitted: false });
     }
 
     render() {
@@ -48,9 +74,31 @@ class EmailCheckContainer extends Component {
                     <p className="content">UBC邮箱</p>
                     <div className="email-check-img"><EmailImg/></div>
                     {
-                        this.state.editing ?
+                        this.state.emailSubmitted ?
+                            <div className="verify-code-container">
+                                <div className="email-display">
+                                    <span className="--email">{`${this.state.email}@alumni.ubc.ca`}</span>
+                                    <FlatButton
+                                        label="修改"
+                                        onClick={this.editEmailHandler}
+                                        style={{minWidth: 0, paddingLeft: 8}}
+                                        labelStyle={{padding: 0}}
+                                    />
+                                </div>
+                                <div className="verification-code">
+                                    <span className="--code">Code: </span>
+                                    <TextField
+                                        id="code"
+                                        style={inputStyle2}
+                                        underlineStyle={underlineStyle2}
+                                        onChange={this.onCodeChange}
+                                        value={this.state.code}
+                                    />
+                                </div>
+                            </div> :
                             <div className="email-input">
                                 <TextField
+                                    id="email"
                                     style={inputStyle}
                                     underlineStyle={underlineStyle}
                                     hintText="输入邮箱"
@@ -58,19 +106,16 @@ class EmailCheckContainer extends Component {
                                     value={this.state.email}
                                 />
                                 <span className="email-suffix">@alumni.ubc.ca</span>
-                            </div> :
-                            <div className="email-display">
-                                <span className="email-suffix">{`${this.state.email}@alumni.ubc.ca`}</span>
-
                             </div>
                     }
                     <RaisedButton
-                        onClick={this.onEmailSubmit}
+                        onClick={this.state.emailSubmitted ? this.onCodeSubmit : this.onEmailSubmit}
                         backgroundColor={PRIMARY_GREEN}
                         fullWidth={true}
                         style={{marginBottom: 12}}
                         label="提交"
                         labelColor={PRIMARY_WHITE}
+                        disabled={this.state.code === "" && this.state.emailSubmitted}
                     />
                     <a href="https://it.ubc.ca/services/email-voice-internet/student-alumni-email-service" className="link">
                         如何获取UBC邮箱
