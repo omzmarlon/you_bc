59a989bc802b1f28c54a62c089129fb41f248ffd
Marlon Ou <omzmarlon@gmail.com>
2018-01-09 17:12:24 -0800

    StudentCardCheck modal

--- web/src/containers/verification/StudentCardCheckContainer.js
+++ web/src/containers/verification/StudentCardCheckContainer.js
@@ -3,12 +3,34 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import VerificationTemplate from "../../components/verification/VerificationTemplate";
+import {RaisedButton} from "material-ui";
+import {GENERAL_TEXT, PRIMARY_GREEN, PRIMARY_WHITE} from "../../styles/constants/colors";
+import StudentCard from "../../components/common/svg/StudentCard";
+import "./StudentCardCheckContainer.less";
 
 class StudentCardCheckContainer extends Component {
     render() {
         return (
             <VerificationTemplate header="验证学生身份">
-                <div>hello</div>
+                <div className={"student-card-check-container"}>
+                    <p className="content">为了确认用户 <span className="highlight">UBC学生身份</span> ，需要上传同学的学生卡</p>
+                    <div className="student-card-check-img"><StudentCard/></div>
+                    <RaisedButton
+                        onClick={() => {}}
+                        backgroundColor={PRIMARY_GREEN}
+                        fullWidth={true}
+                        style={{marginBottom: 12}}
+                        label="上传"
+                        labelColor={PRIMARY_WHITE}
+                    />
+                    <RaisedButton
+                        onClick={() => {}}
+                        fullWidth={true}
+                        style={{marginBottom: 12}}
+                        label="其他方式验证"
+                        labelColor={GENERAL_TEXT}
+                    />
+                </div>
             </VerificationTemplate>
         )
     }
