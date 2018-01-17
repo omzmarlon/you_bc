3ac4c9216b6c0feaf119ee7043b8e0fbd36000d6
tomyang729 <tomatoyang729@gmail.com>
2018-01-17 15:26:39 -0800

    half way

---
+++ src/main/java/com/youbc/controllers/protected_api/ClassmatesController.java
@@ -0,0 +1,7 @@
+package com.youbc.controllers.protected_api;
+
+/**
+ * Created by yifanyang on 2018-01-17.
+ */
+public class ClassmatesController {
+}
---
+++ src/main/java/com/youbc/database/ClassmateDAO.java
@@ -0,0 +1,7 @@
+package com.youbc.database;
+
+/**
+ * Created by yifanyang on 2018-01-17.
+ */
+public class ClassmateDAO {
+}
