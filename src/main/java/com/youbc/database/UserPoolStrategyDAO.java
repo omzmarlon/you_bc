2d93c84af0972b13cb649bd991eea1fc1378b3e8
Marlon Ou <omzmarlon@gmail.com>
2018-01-21 22:26:38 -0800

    Liked/Disliked user should not appear

--- src/main/java/com/youbc/database/UserPoolStrategyDAO.java
+++ src/main/java/com/youbc/database/UserPoolStrategyDAO.java
@@ -4,6 +4,7 @@ import org.jooq.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -14,6 +15,10 @@ import static com.youbc.generated.schema.tables.FriendsProfile.FRIENDS_PROFILE;
 import static com.youbc.generated.schema.tables.RoommatesLikes.ROOMMATES_LIKES;
 import static com.youbc.generated.schema.tables.RoommatesProfile.ROOMMATES_PROFILE;
 import static com.youbc.generated.schema.tables.UserProfile.USER_PROFILE;
+
+import static com.youbc.generated.schema.tables.ClassmatesDislikes.CLASSMATES_DISLIKES;
+import static com.youbc.generated.schema.tables.FriendsDislikes.FRIENDS_DISLIKES;
+import static com.youbc.generated.schema.tables.RoommatesDislikes.ROOMMATES_DISLIKES;
 
 @Component
 public class UserPoolStrategyDAO {
@@ -26,16 +31,27 @@ public class UserPoolStrategyDAO {
     }
 
     public List<String> fetchRandomClassmates(String userId, Integer amount, Integer gender, Set<String> except) {
+        Set<String> likedUserIds = dslContext.select()
+                .from(CLASSMATES_LIKES)
+                .where(CLASSMATES_LIKES.LIKER.eq(userId))
+                .fetchSet(CLASSMATES_LIKES.LIKEE);
+        Set<String> dislikedUserIds = dslContext.select()
+                .from(CLASSMATES_DISLIKES)
+                .where(CLASSMATES_DISLIKES.DISLIKER.eq(userId))
+                .fetchSet(CLASSMATES_DISLIKES.DISLIKEE);
         if (gender == 0) {
-            return dslContext
+            List<String> result = dslContext
                     .select()
                     .from(CLASSMATES_PROFILE.leftJoin(USER_PROFILE).on(CLASSMATES_PROFILE.USER_ID.eq(USER_PROFILE.USER_ID)))
                     .where(CLASSMATES_PROFILE.USER_ID.notEqual(userId))
                     .and(CLASSMATES_PROFILE.USER_ID.notIn(except))
                     .limit(amount)
                     .fetch(CLASSMATES_PROFILE.USER_ID);
+            result.removeAll(likedUserIds);
+            result.removeAll(dislikedUserIds);
+            return result;
         } else {
-            return dslContext
+            List<String> result = dslContext
                     .select()
                     .from(CLASSMATES_PROFILE.leftJoin(USER_PROFILE).on(CLASSMATES_PROFILE.USER_ID.eq(USER_PROFILE.USER_ID)))
                     .where(CLASSMATES_PROFILE.USER_ID.notEqual(userId))
@@ -43,20 +59,34 @@ public class UserPoolStrategyDAO {
                     .and(CLASSMATES_PROFILE.USER_ID.notIn(except))
                     .limit(amount)
                     .fetch(CLASSMATES_PROFILE.USER_ID);
+            result.removeAll(likedUserIds);
+            result.removeAll(dislikedUserIds);
+            return result;
         }
     }
 
     public List<String> fetchRandomfriends(String userId, Integer amount, Integer gender, Set<String> except) {
+        Set<String> likedUserIds = dslContext.select()
+                .from(FRIENDS_LIKES)
+                .where(FRIENDS_LIKES.LIKER.eq(userId))
+                .fetchSet(FRIENDS_LIKES.LIKEE);
+        Set<String> dislikedUserIds = dslContext.select()
+                .from(FRIENDS_DISLIKES)
+                .where(FRIENDS_DISLIKES.DISLIKER.eq(userId))
+                .fetchSet(FRIENDS_DISLIKES.DISLIKEE);
         if (gender == 0) {
-            return dslContext
+            List<String> result = dslContext
                     .select()
                     .from(FRIENDS_PROFILE.leftOuterJoin(USER_PROFILE).on(FRIENDS_PROFILE.USER_ID.eq(USER_PROFILE.USER_ID)))
                     .where(FRIENDS_PROFILE.USER_ID.notEqual(userId))
                     .and(FRIENDS_PROFILE.USER_ID.notIn(except))
                     .limit(amount)
                     .fetch(FRIENDS_PROFILE.USER_ID);
+            result.removeAll(likedUserIds);
+            result.removeAll(dislikedUserIds);
+            return result;
         } else {
-            return dslContext
+            List<String> result = dslContext
                     .select()
                     .from(FRIENDS_PROFILE.leftOuterJoin(USER_PROFILE).on(FRIENDS_PROFILE.USER_ID.eq(USER_PROFILE.USER_ID)))
                     .where(USER_PROFILE.SEX.eq(gender))
@@ -64,20 +94,34 @@ public class UserPoolStrategyDAO {
                     .and(FRIENDS_PROFILE.USER_ID.notIn(except))
                     .limit(amount)
                     .fetch(FRIENDS_PROFILE.USER_ID);
+            result.removeAll(likedUserIds);
+            result.removeAll(dislikedUserIds);
+            return result;
         }
     }
 
     public List<String> fetchRandomRoommates(String userId, Integer amount, Integer gender, Set<String> except) {
+        Set<String> likedUserIds = dslContext.select()
+                .from(ROOMMATES_LIKES)
+                .where(ROOMMATES_LIKES.LIKER.eq(userId))
+                .fetchSet(ROOMMATES_LIKES.LIKEE);
+        Set<String> dislikedUserIds = dslContext.select()
+                .from(ROOMMATES_DISLIKES)
+                .where(ROOMMATES_DISLIKES.DISLIKER.eq(userId))
+                .fetchSet(ROOMMATES_DISLIKES.DISLIKEE);
         if (gender == 0) {
-            return dslContext
+            List<String> result =  dslContext
                     .select()
                     .from(ROOMMATES_PROFILE.leftOuterJoin(USER_PROFILE).on(ROOMMATES_PROFILE.USER_ID.eq(USER_PROFILE.USER_ID)))
                     .where(ROOMMATES_PROFILE.USER_ID.notEqual(userId))
                     .and(ROOMMATES_PROFILE.USER_ID.notIn(except))
                     .limit(amount)
                     .fetch(ROOMMATES_PROFILE.USER_ID);
+            result.removeAll(likedUserIds);
+            result.removeAll(dislikedUserIds);
+            return result;
         } else {
-            return dslContext
+            List<String> result =  dslContext
                     .select()
                     .from(ROOMMATES_PROFILE.leftOuterJoin(USER_PROFILE).on(ROOMMATES_PROFILE.USER_ID.eq(USER_PROFILE.USER_ID)))
                     .where(USER_PROFILE.SEX.eq(gender))
@@ -85,20 +129,34 @@ public class UserPoolStrategyDAO {
                     .and(ROOMMATES_PROFILE.USER_ID.notIn(except))
                     .limit(amount)
                     .fetch(ROOMMATES_PROFILE.USER_ID);
+            result.removeAll(likedUserIds);
+            result.removeAll(dislikedUserIds);
+            return result;
         }
     }
 
     public List<String> fetchClassmatesByLikes(String userId, Integer amount, Integer gender, Set<String> except) {
+        Set<String> likedUserIds = dslContext.select()
+                .from(CLASSMATES_LIKES)
+                .where(CLASSMATES_LIKES.LIKER.eq(userId))
+                .fetchSet(CLASSMATES_LIKES.LIKEE);
+        Set<String> dislikedUserIds = dslContext.select()
+                .from(CLASSMATES_DISLIKES)
+                .where(CLASSMATES_DISLIKES.DISLIKER.eq(userId))
+                .fetchSet(CLASSMATES_DISLIKES.DISLIKEE);
         if (gender == 0) {
-            return dslContext
+            List<String> result = dslContext
                     .select()
                     .from(CLASSMATES_LIKES.leftOuterJoin(USER_PROFILE).on(CLASSMATES_LIKES.LIKER.eq(USER_PROFILE.USER_ID)))
                     .where(CLASSMATES_LIKES.LIKEE.eq(userId))
                     .and(CLASSMATES_LIKES.LIKER.notIn(except))
                     .limit(amount)
                     .fetch(CLASSMATES_LIKES.LIKER);
+            result.removeAll(likedUserIds);
+            result.removeAll(dislikedUserIds);
+            return result;
         } else {
-            return dslContext
+            List<String> result = dslContext
                     .select()
                     .from(CLASSMATES_LIKES.leftOuterJoin(USER_PROFILE).on(CLASSMATES_LIKES.LIKER.eq(USER_PROFILE.USER_ID)))
                     .where(USER_PROFILE.SEX.eq(gender))
@@ -106,20 +164,34 @@ public class UserPoolStrategyDAO {
                     .and(CLASSMATES_LIKES.LIKER.notIn(except))
                     .limit(amount)
                     .fetch(CLASSMATES_LIKES.LIKER);
+            result.removeAll(likedUserIds);
+            result.removeAll(dislikedUserIds);
+            return result;
         }
     }
 
     public List<String> fetchFriendsByLikes(String userId, Integer amount, Integer gender, Set<String> except) {
+        Set<String> dislikedUserIds = dslContext.select()
+                .from(FRIENDS_DISLIKES)
+                .where(FRIENDS_DISLIKES.DISLIKER.eq(userId))
+                .fetchSet(FRIENDS_DISLIKES.DISLIKEE);
+        Set<String> likedUserIds = dslContext.select()
+                .from(FRIENDS_LIKES)
+                .where(FRIENDS_LIKES.LIKER.eq(userId))
+                .fetchSet(FRIENDS_LIKES.LIKEE);
         if (gender == 0) {
-            return dslContext
+            List<String> result =  dslContext
                     .select()
                     .from(FRIENDS_LIKES.leftOuterJoin(USER_PROFILE).on(FRIENDS_LIKES.LIKER.eq(USER_PROFILE.USER_ID)))
                     .where(FRIENDS_LIKES.LIKEE.eq(userId))
                     .and(FRIENDS_LIKES.LIKER.notIn(except))
                     .limit(amount)
                     .fetch(FRIENDS_LIKES.LIKER);
+            result.removeAll(likedUserIds);
+            result.removeAll(dislikedUserIds);
+            return result;
         } else {
-            return dslContext
+            List<String> result =  dslContext
                     .select()
                     .from(FRIENDS_LIKES.leftOuterJoin(USER_PROFILE).on(FRIENDS_LIKES.LIKER.eq(USER_PROFILE.USER_ID)))
                     .where(USER_PROFILE.SEX.eq(gender))
@@ -127,20 +199,34 @@ public class UserPoolStrategyDAO {
                     .and(FRIENDS_LIKES.LIKER.notIn(except))
                     .limit(amount)
                     .fetch(FRIENDS_LIKES.LIKER);
+            result.removeAll(likedUserIds);
+            result.removeAll(dislikedUserIds);
+            return result;
         }
     }
 
     public List<String> fetchRoommatesByLikes(String userId, Integer amount, Integer gender, Set<String> except) {
+        Set<String> likedUserIds = dslContext.select()
+                .from(ROOMMATES_LIKES)
+                .where(ROOMMATES_LIKES.LIKER.eq(userId))
+                .fetchSet(ROOMMATES_LIKES.LIKEE);
+        Set<String> dislikedUserIds = dslContext.select()
+                .from(ROOMMATES_DISLIKES)
+                .where(ROOMMATES_DISLIKES.DISLIKER.eq(userId))
+                .fetchSet(ROOMMATES_DISLIKES.DISLIKEE);
         if (gender == 0) {
-            return dslContext
+            List<String> result = dslContext
                     .select()
                     .from(ROOMMATES_LIKES.leftOuterJoin(USER_PROFILE).on(ROOMMATES_LIKES.LIKER.eq(USER_PROFILE.USER_ID)))
                     .where(ROOMMATES_LIKES.LIKEE.eq(userId))
                     .and(ROOMMATES_LIKES.LIKER.notIn(except))
                     .limit(amount)
                     .fetch(ROOMMATES_LIKES.LIKER);
+            result.removeAll(likedUserIds);
+            result.removeAll(dislikedUserIds);
+            return result;
         } else {
-            return dslContext
+            List<String> result = dslContext
                     .select()
                     .from(ROOMMATES_LIKES.leftOuterJoin(USER_PROFILE).on(ROOMMATES_LIKES.LIKER.eq(USER_PROFILE.USER_ID)))
                     .where(USER_PROFILE.SEX.eq(gender))
@@ -148,6 +234,9 @@ public class UserPoolStrategyDAO {
                     .and(ROOMMATES_LIKES.LIKER.notIn(except))
                     .limit(amount)
                     .fetch(ROOMMATES_LIKES.LIKER);
+            result.removeAll(likedUserIds);
+            result.removeAll(dislikedUserIds);
+            return result;
         }
     }
 }
