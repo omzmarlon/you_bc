4fbebda1f527eb728f2573cd0fa09047814e466d
Marlon Ou <omzmarlon@gmail.com>
2018-05-20 13:28:49 -0700

    fix jooq exception translator

--- you_bc-service/src/main/java/com/youbc/services/JooqExecutionListener.java
+++ you_bc-service/src/main/java/com/youbc/services/JooqExecutionListener.java
@@ -1,10 +1,10 @@
 package com.youbc.services;
 
+import com.youbc.error_handling.YouBCError;
+import com.youbc.error_handling.YouBCException;
 import org.jooq.ExecuteContext;
-import org.jooq.SQLDialect;
 import org.jooq.impl.DefaultExecuteListener;
-import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
-import org.springframework.jdbc.support.SQLExceptionTranslator;
+import org.springframework.http.HttpStatus;
 
 public class JooqExecutionListener extends DefaultExecuteListener {
     /*
@@ -12,15 +12,12 @@ public class JooqExecutionListener extends DefaultExecuteListener {
     * */
     @Override
     public void exception(ExecuteContext context) {
-        //We SHOULD HAVE this translator because SpringFramework has more detailed/meaningful exceptions
-        SQLDialect dialect = context.configuration().dialect();
-        SQLExceptionTranslator translator
-                = new SQLErrorCodeSQLExceptionTranslator(dialect.name());
         context.exception(
-                translator.translate(
-                        "Accessing database using jOOQ",
-                        context.sql(), context.sqlException()
-                )
+                new YouBCException(new YouBCError(
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                        "Database Error",
+                        context.sqlException().getMessage()
+                ))
         );
     }
 }
