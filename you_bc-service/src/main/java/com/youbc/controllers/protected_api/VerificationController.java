e006cd9d55e4ec00ceac1fa7cc82f11adcd3cfc7
tomyang729 <tomatoyang729@gmail.com>
2018-05-24 00:33:43 -0700

    add logger

--- you_bc-service/src/main/java/com/youbc/controllers/protected_api/VerificationController.java
+++ you_bc-service/src/main/java/com/youbc/controllers/protected_api/VerificationController.java
@@ -6,6 +6,8 @@ import com.youbc.requests.VerificationRequest;
 import com.youbc.response.VerificationResponse;
 import com.youbc.services.verification.VerificationService;
 import com.youbc.utilities.Endpoints;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VerificationController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VerificationController.class);
 
     private VerificationDAO verificationDAO;
     private VerificationService verificationService;
@@ -30,6 +34,9 @@ public class VerificationController {
 
     @PostMapping( value = Endpoints.VERIFICATION )
     public VerificationResponse verifiyCode(@RequestBody VerificationRequest request) {
+
+        LOGGER.debug("Handle Verification request {}", request);
+
         Integer userID = (Integer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         boolean codeMatch = verificationService.approve(request.getVerificationCode());
         if (codeMatch) {
@@ -45,6 +52,9 @@ public class VerificationController {
 
     @GetMapping( value = Endpoints.VERIFICATION )
     public VerificationResponse getVerificationResult() {
+
+        LOGGER.debug("Get Verification Status request {}");
+
         Integer userID = (Integer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         // todo: db exception handling
         boolean approved = verificationDAO.fetchVerificationStatus(userID);
